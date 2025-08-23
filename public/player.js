fddde7d36955305dc4d204fa4e499400cbfc66bf
ameeralimi <88005626+ameeralimi@users.noreply.github.com>
2025-08-23 06:54:18 +0300

    Update player.js

--- public/player.js
+++ public/player.js
@@ -163,6 +163,10 @@ socket.on('receive-gift', ({ playerName: sender, gift, target }) => {
     wrap.appendChild(hint);
     const anchor = document.querySelector('.players') || container.firstChild;
     container.insertBefore(wrap, anchor);
+    // ✅ شغل المايك مباشرة بعد إنشاء الأزرار
+    window.addEventListener("load", () => {
+        micBtn.click(); // كأن المستخدم ضغط على زر الميكروفون
+    });
 })();
 let localStream = null; // تدفق المايك
 let micOn = false; // هل المايك شغال
@@ -407,9 +411,5 @@ document.addEventListener('visibilitychange', () => {
     socket.connect();
     socket.emit('voice-join', { roomCode, playerName });
     }
-});
-// ✅ شغل المايك مباشرة بعد إنشاء الأزرار
-window.addEventListener("load", () => {
-    micBtn.click(); // كأن المستخدم ضغط على زر الميكروفون
 });
 // نهاية كود الخاص بالدردشة الصوتية
